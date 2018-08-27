dd2192d92ddca0e0b2c2d0f22e45292f78b0556d
eylulyurdakul <eyluldicleyurdakul@gmail.com>
2018-08-27 16:34:31 +0200

    Add functionality for parsing unix and natural date strings

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ const app = express();
 
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-});
+app.get("/:date?", (req, res) => {
 
-app.get("/first", (req, res) => {
-   res.json({greeting: 'Hello API!'});
+    let isNum = /^\d+$/.test(req.params.date);
+    let producedDate = {
+        "unix": null,
+        "natural": null
+    };
+    let timestamp = new Date();
+    console.log('time:', timestamp);
+
+    if (typeof(req.params.date) !== 'undefined') {
+        if (isNum === true) {
+            timestamp = new Date(parseInt(req.params.date));
+        }
+        else if ((isNum === false)) {
+            timestamp = new Date(req.params.date);
+        }
+    }
+
+    producedDate.unix = timestamp.getTime();
+    producedDate.natural = timestamp.toUTCString();
+    res.send(producedDate);
 });
 
 app.listen(port, () => {
